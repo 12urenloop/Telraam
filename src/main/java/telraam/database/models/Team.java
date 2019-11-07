d466a8dd26055e151552c12e54ccfbd623ab6b26
hannes <hannes>
2019-11-07 16:47:08 +0100

    Filled in Team class

--- src/main/java/telraam/database/models/Team.java
+++ src/main/java/telraam/database/models/Team.java
@@ -1,4 +1,36 @@
 package telraam.database.models;
 
 public class Team {
+    private Integer id;
+    private String name;
+    private Integer baton_id;
+
+    public Team(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getBaton_id() {
+        return baton_id;
+    }
+
+    public void setBaton_id(Integer baton_id) {
+        this.baton_id = baton_id;
+    }
 }
