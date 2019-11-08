dafdb3fe3f72f0fe27c19b34d03ede229080ff36
ajuvercr <arthur.vercruysse@ugent.be>
2019-11-08 03:12:32 +0100

    js is better for this <3

---
+++ hw_sim/main.js
@@ -0,0 +1,87 @@
+'use strict';
+
+const ArgumentParser = require('argparse').ArgumentParser;
+const net = require('net');
+
+// Standard Normal variate using Box-Muller transform.
+function randn_bm(mean, dev) {
+    var u = 0,
+        v = 0;
+    while (u === 0) u = Math.random(); //Converting [0,1) to (0,1)
+    while (v === 0) v = Math.random();
+    const out = Math.sqrt(-2.0 * Math.log(u)) * Math.cos(2.0 * Math.PI * v);
+    return out * dev + mean;
+}
+
+class Beacon {
+    constructor(id, host, port) {
+        this.socket = net.Socket();
+        this.socket.connect(port, host);
+        this.id = id;
+    }
+
+    send(id) {
+        const buffer = Buffer.alloc(10);
+        console.log("Beacon", id, "Runner", this.id);
+        this.socket.write(buffer);
+    }
+}
+
+class Runner {
+    constructor(id, mean, dev, beacons) {
+        this.mean = mean;
+        this.dev = dev;
+        this.beacons = beacons;
+        this.at = 0;
+        this.id = id;
+
+        this.run = this.send.bind(this);
+
+        this.set_next();
+    }
+
+    send() {
+        this.beacons[this.at].send(this.id);
+        this.at++;
+
+        if (this.at >= this.beacons.length) {
+            this.at = 0;
+        }
+
+        this.set_next();
+    }
+
+    set_next() {
+        const time_till_next = randn_bm(this.mean, this.dev);
+        setTimeout(this.run, time_till_next);
+    }
+}
+
+function main() {
+    const parser = new ArgumentParser({
+        version: '0.0.1',
+        addHelp: true,
+        description: 'Argparse example'
+    });
+    parser.addArgument(['-p', "--port"], { defaultValue: 4564, type: "int", help: "Port to use" });
+
+    parser.addArgument(['-a', "--address"], { defaultValue: "127.0.0.1", help: "Ip address to test" });
+    parser.addArgument(['-b', "--beacons"], { defaultValue: 2, type: "int", help: "Amount of beacons" });
+    parser.addArgument(['-r', "--runners"], { defaultValue: 5, type: "int", help: "Amount of runners" });
+    parser.addArgument(['-m', "--mean"], { defaultValue: 500, type: "int", help: "Mean of runner speed (ms per round)" });
+    parser.addArgument(['-d', "--runner_deviation"], { defaultValue: 10, type: "int", help: "Standard deviation of runner speed (per runner)" });
+    parser.addArgument(['-D', "--round_deviation"], { defaultValue: 0, type: "int", help: "Standard deviation of runner speed (per round)" });
+
+    const args = parser.parseArgs();
+
+    const beacons = [];
+    for (let i = 0; i < args.beacons; i++) {
+        beacons.push(new Beacon(i + 1, args.address, args.port));
+    }
+
+    const runners = [];
+    for (let i = 0; i < args.runners; i++) {
+        runners.push(new Runner(i + 1, randn_bm(args.mean, args.runner_deviation), args.round_deviation, beacons));
+    }
+}
+main();
--- src/main/java/telraam/beacon/BeaconMessage.java
+++ src/main/java/telraam/beacon/BeaconMessage.java
@@ -1,23 +1,29 @@
 package telraam.beacon;
 
 /**
-* BeaconMessage is the representation of what is received from a beacon.
-* This should parse the incoming byte[].
-*
-* @author  Arthur Vercruysse
-*/
+ * BeaconMessage is the representation of what is received from a beacon. This
+ * should parse the incoming byte[].
+ *
+ * @author Arthur Vercruysse
+ */
 public class BeaconMessage {
-    public static final int MESSAGESIZE = 10;
+    public static final int MESSAGESIZE = Byte.BYTES + Byte.BYTES + Long.BYTES;
 
-    public byte[] data;
+    public byte beaconTag;
+    public byte batonTag;
+    public long timestamp;
 
     // DO NOT STORE THIS DATA, IT WILL BE OVERWRITTEN
     public BeaconMessage(byte[] data) {
-        this.data = data.clone();
+        beaconTag = data[0];
+        batonTag = data[1];
+        timestamp = ((long) data[9] << 56) | ((long) data[8] & 0xff) << 48 | ((long) data[7] & 0xff) << 40
+                | ((long) data[6] & 0xff) << 32 | ((long) data[5] & 0xff) << 24 | ((long) data[4] & 0xff) << 16
+                | ((long) data[3] & 0xff) << 8 | ((long) data[2] & 0xff);
     }
 
     @Override
     public String toString() {
-        return new String(this.data);
+        return String.format("Beacon %o: runner: %o at %d", this.beaconTag, this.batonTag, this.timestamp);
     }
 }
