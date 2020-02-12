48f8459e1e07dea9881fc73ef5b009b00f029a36
abeforgit <arnebertrand@gmail.com>
2020-02-12 14:23:28 +0100

    Launch both threads

--- src/main/java/telraam/App.java
+++ src/main/java/telraam/App.java
@@ -3,22 +3,26 @@ package telraam;
 import io.dropwizard.Application;
 import io.dropwizard.jdbi3.JdbiFactory;
 import io.dropwizard.jdbi3.bundles.JdbiExceptionsBundle;
+import io.dropwizard.jersey.setup.JerseyEnvironment;
 import io.dropwizard.setup.Bootstrap;
 import io.dropwizard.setup.Environment;
+
+import java.io.IOException;
+import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
+
 import org.jdbi.v3.core.Jdbi;
-import telraam.api.BatonResource;
-import telraam.api.HelloworldResource;
+import telraam.api.*;
 import telraam.beacon.BeaconAggregator;
-import telraam.database.daos.BatonDAO;
+import telraam.beacon.BeaconMessage;
+import telraam.database.daos.*;
 import telraam.database.models.Baton;
+import telraam.database.models.Detection;
 import telraam.database.models.Id;
 import telraam.healthchecks.TemplateHealthCheck;
-
-
 
 
 public class App extends Application<AppConfiguration> {
@@ -28,13 +32,7 @@ public class App extends Application<AppConfiguration> {
     private Jdbi database;
 
     public static void main(String[] args) throws Exception {
-        BeaconAggregator ba = new BeaconAggregator(4564);
-        ba.onError((e) -> {logger.warning(e.getMessage()); return null;});
-        ba.onData((e) -> {logger.info(e.toString()); return null;});
-        ba.onConnect((_e) -> {logger.info("Connect"); return null;});
-        ba.onDisconnect((_e) -> {logger.info("Disconnected"); return null;});
-        // new App().run(args);
-        ba.run();
+        new App().run(args);
     }
 
     @Override
@@ -50,26 +48,50 @@ public class App extends Application<AppConfiguration> {
 
 
     @Override
-    public void run(AppConfiguration configuration, Environment environment) throws Exception {
+    public void run(AppConfiguration configuration, Environment environment)
+            throws IOException {
         this.config = configuration;
         this.environment = environment;
         // Add database
         final JdbiFactory factory = new JdbiFactory();
-        database = factory.build(environment, configuration.getDataSourceFactory(), "postgresql");
+        database =
+                factory.build(environment, configuration.getDataSourceFactory(),
+                        "postgresql");
 
 
         // Add api resources
-        final HelloworldResource resource = new HelloworldResource(
-                configuration.getTemplate(),
-                configuration.getDefaultName()
-        );
-        environment.jersey().register(resource);
-        environment.jersey().register(new BatonResource(database.onDemand(BatonDAO.class)));
-
+        JerseyEnvironment jersey = environment.jersey();
+        jersey.register(new BatonResource(database.onDemand(BatonDAO.class)));
+        jersey.register(new BeaconResource(database.onDemand(BeaconDAO.class)));
+        jersey.register(
+                new DetectionResource(database.onDemand(DetectionDAO.class)));
+        jersey.register(new LapResource(database.onDemand(LapDAO.class)));
+        jersey.register(new TeamResource(database.onDemand(TeamDAO.class)));
         // Register healthcheck
         // environment.healthChecks().register("database", new DatabaseHealthCheck(database));
-        environment.healthChecks().register("template", new TemplateHealthCheck(configuration.getTemplate()));
+        environment.healthChecks().register("template",
+                new TemplateHealthCheck(configuration.getTemplate()));
         logger.warning("TEST LOG");
+
+        BeaconAggregator ba = new BeaconAggregator(4564);
+        ba.onError(e -> {
+            logger.warning(e.getMessage());
+            return null;
+        });
+        ba.onData(e -> {
+            logger.info(e.toString());
+            return null;
+        });
+        ba.onConnect(_e -> {
+            logger.info("Connect");
+            return null;
+        });
+        ba.onDisconnect(_e -> {
+            logger.info("Disconnected");
+            return null;
+        });
+        Thread beaconMessages = new Thread(ba);
+        beaconMessages.start();
     }
 
     public AppConfiguration getConfig() {
