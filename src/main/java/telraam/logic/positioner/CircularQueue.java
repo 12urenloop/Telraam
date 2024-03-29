f71ad1ba1f9e0c4791352ffbe09553693d8fcede
Topvennie <vincent@vallaeys.com>
2024-03-29 14:21:09 +0100

    use built in function

--- src/main/java/telraam/logic/positioner/CircularQueue.java
+++ src/main/java/telraam/logic/positioner/CircularQueue.java
@@ -5,26 +5,17 @@ import java.util.LinkedList;
 public class CircularQueue<T> extends LinkedList<T> {
 
     private final int maxSize;
-    private int size = 0;
-
     public CircularQueue(int maxSize) {
         this.maxSize = maxSize;
     }
 
     @Override
     public boolean add(T e) {
-        if (this.size >= this.maxSize) {
+        if (size() >= this.maxSize) {
             removeFirst();
-            this.size--;
         }
 
-        boolean result = super.add(e);
-
-        if (result) {
-            this.size++;
-        }
-
-        return result;
+        return super.add(e);
     }
 
 }
