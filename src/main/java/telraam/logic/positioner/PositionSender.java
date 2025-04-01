f22a61fe320077aeb342b852222baf4b92e7ab4d
Topvennie <vincent@vallaeys.com>
2025-04-01 22:31:47 +0200

    adjust position message

--- src/main/java/telraam/logic/positioner/PositionSender.java
+++ src/main/java/telraam/logic/positioner/PositionSender.java
@@ -4,16 +4,22 @@ import telraam.websocket.WebSocketMessage;
 import telraam.websocket.WebSocketMessageSingleton;
 
 import java.util.List;
+import java.util.Map;
 
 public class PositionSender {
     private final WebSocketMessage<List<Position>> message = new WebSocketMessage<>();
+    private final String name;
 
     public PositionSender(String name) {
-        this.message.setTopic("position_" + name);
+        this.message.setTopic("position");
+        this.name = name;
     }
 
     public void send(List<Position> positions) {
-        this.message.setData(positions);
+        Map<String, Object> data = Map.of(
+                "positioner", this.name,
+                "positions", positions
+        );
         WebSocketMessageSingleton.getInstance().sendToAll(this.message);
     }
 
